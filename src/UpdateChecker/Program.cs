d9dd9c2652a0c7855d0e78dbfbcb2b394b3ce11a
Pedro <185304469+alvespedro0225@users.noreply.github.com>
2025-06-13 13:19:30 -0300

    fixed errors

--- src/UpdateChecker/Program.cs
+++ src/UpdateChecker/Program.cs
@@ -4,24 +4,26 @@ using Application.Services.Implementations;
 using Infrastructure.Files;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using UpdateChecker;
 
-var builder = Host.CreateDefaultBuilder(args);
+var builder = Host.CreateApplicationBuilder(args);
 
-builder.ConfigureServices(services => services
+builder.Services
         .AddScoped<IFileService, FileService>()
         .AddScoped<IMangadexService, MangadexService>()
         .AddScoped<INotificationService, EmailService>()
-        .AddHostedService<App>()
-        );
+        .AddHostedService<App>();
 
-builder.ConfigureServices(services => services
+builder.Services
         .AddHttpClient("Mangadex", client =>
         {
                 client.DefaultRequestHeaders.UserAgent.Add(
                         new ProductInfoHeaderValue("UpdateChecker", "1.0"));
-        }));
-
-// TODO add logging and testing, review models
-await builder.RunConsoleAsync();
+        });
+// appsettings.json not working fix later
+builder.Logging.SetMinimumLevel(LogLevel.Error);
+// TODO add testing, review models
+var app = builder.Build();
+await app.RunAsync();
 
